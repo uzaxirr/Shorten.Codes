a6a012a3314e39bf2b2406235cad7e35a13629c6
sayamsamal <samal.sayam@gmail.com>
2021-08-27 23:23:21 +0530

    backend optimisation v2

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -5,12 +5,7 @@ const bcrypt        =   require("bcrypt");
 const UserSchema    =   require("../models/usermodel");
 const router        =   express.Router();
 
-
 SECRET = process.env.SEC;
-
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../src/login.html"));
-});
 
 router.post("/", async (req, res) => {
   let usrname = req.body.username;
@@ -18,18 +13,22 @@ router.post("/", async (req, res) => {
 
   const user = await UserSchema.findOne({ username: usrname });
   if (!user) {
-    res.send({ status: 404, message: "User Not Found" });
+    return res.status(404).send({message: "User Not Found"});
   }
 
   if (await bcrypt.compare(pswrd, user.password)) {
     const token = jwt.sign({ un: usrname }, SECRET);
     res.cookie("token", token, { maxAge: 900000, httpOnly: true });
     console.log("USER NAME OBTINEED: ", usrname);
-    res.send({
-      status: 200,
+    
+    res.status(200).send({
+      isLoggedin: true,
+      userName: usrname,
       LoginToken: token,
       message: "Logged In Sucessfully",
     });
+  } else {
+    return res.status(400).send({message: "Incorrect Password"});
   }
 
   console.log(usrname);
